a3c36bd1eefaa0010bc8774e9e7b9069c0938fac
Nadezhda Traykova <n.l.traykova@gmail.com>
2026-07-04 18:31:56 +0300

    add title to create artist page

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -4,7 +4,7 @@ import artistService from "../services/artistService";
 const artistController = Router();
 
 artistController.get('/create', (req, res) => {
-    res.render('artists/create');
+    res.render('artists/create', { pageTitle: 'Create artist' });
 });
 
 artistController.post('/create', async (req, res) =>{
